9691908ac390a035a6e9bec54af2e1345e569bf4
vKulsan <w14477942@gmail.com>
2024-10-15 16:41:09 +0300

    Init project

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+      <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+const links = [
+  {
+    label: "Home",
+    to: "/"
+  },
+  {
+    label: "My book",
+    to: "/mybook"
+  }
+]
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <body>
+    <div class="news">
+        <div class="newsBlock">
+            <p style="font-size: 30px; font-weight: 550;">
+                А что там такое?
+            </p>
+            <p>
+                Сегодня (05.06.24) значимый день для нас!
+
+                Мы изменяем название и оформление бренда. Было Gland Studio стало Van's Studio.
+
+                Мы решили, что пришло время для обновлений, чтобы лучше соответствовать нашим ценностям и видению. Новое название Van's Studio отражает наше стремление к инновациям и качеству. Мы уверены, что это изменение поможет, а также привлечет новую аудиторию.
+
+                Кроме того, мы обновляем оформление бренда, чтобы сделать его более современным и привлекательным. Новая цветовая палитра, логотип и визуальные элементы будут отражать нашу приверженность к совершенству и креативности. Мы надеемся, что эти изменения помогут нам выделиться на рынке и укрепить нашу позицию как лидера в индустрии.
+            </p>
+        </div>
+    </div>
+    <div class="project" style="background-image: url(My_book_1x.png);
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-attachment: fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;">
+        <div class="project-zagolov">
+            <a href="mybook.html">My book</a>
+        </div>
+        <div>
+            My book — небольшая програма для создания текста и написания скриптов. Функционал: Создание .txt текстов, .cs скриптов
+        </div>
+    </div>
+    <div class="project" style="background-image: url(vapertuscreen.png);
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-attachment: fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;">
+        <div class="project-zagolov">
+            <a href="mybook.html">Vapertu</a>
+        </div>
+        <div>
+            Vapertu - это игра от нашей студии. Она есть на GameJolt, а также находиться на стадии разработки
+        </div>
+    </div>
+</body>
+</template>
